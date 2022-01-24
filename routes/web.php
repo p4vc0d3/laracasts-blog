fd98888211ddb26a1c4ec879db4243d4bab6f6ea
Paul Panata <64294932+p4vc0d3@users.noreply.github.com>
2022-01-24 14:53:46 -0500

    Setup authorization for admin-specific links

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,6 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 Route::post('newsletter', NewsletterController::class);
 
 // Admin
-Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
+Route::middleware('can:admin')->group(function () {
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+});
